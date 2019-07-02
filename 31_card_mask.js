cf486768b006bc1bb41298f31a5ecd83d2cbc9bc
Glen Johnson <glenjohnson39@yahoo.com.au>
2019-07-03 09:01:17 +1000

    31, will add testing

--- 31_card_mask.js
+++ 31_card_mask.js
@@ -1,7 +1,7 @@
 //Credit Card Mask
 /*
-Problem Description: Usually when you buy something, you're asked whether your credit card number, phone number or 
-answer to your most secret question is still correct. However, since someone could look over your shoulder, you 
+Problem Description: Usually when you buy something, you're asked whether your credit card number, phone number or
+answer to your most secret question is still correct. However, since someone could look over your shoulder, you
 don't want that shown on your screen. Instead, we mask it.
 Your task is to write a function maskify, which changes all but the last four characters into '#'.
 Examples
@@ -13,3 +13,7 @@ maskify(                "") ==                 ""
 maskify("Skippy")                                   == "##ippy"
 maskify("Nananananananananananananananana Batman!") == "######################################man!"
 */
+
+function maskify(input) {
+  return input.split('').fill('#', 0, input.length-4).join('')
+}
